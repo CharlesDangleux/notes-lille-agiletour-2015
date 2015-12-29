d6b81ffd4422f302f16be2a7ea0487e3d8272b50
TGITS <thegeekintheshell@gmail.com>
2015-12-29 15:53:21 +0100

    Modification de la configuration de gaiden

--- config.groovy
+++ config.groovy
@@ -1,11 +1,19 @@
 // The title of this document
-//title = "Notes Lille AgileTour 2015"
+title = "Notes AgileTour Lille 2015"
 
 // The version of this document
-//version = "0.1"
+version = "0.1"
 
 // The path of source files directory
-//sourceDirectoryPath = "."
+sourceDirectoryPath = "."
 
 // The path of directory to be outputted a document
-//outputDirectoryPath = "build"
+outputDirectoryPath = "build"
+
+theme = "default"
+layout = "main"
+inputEncoding = "UTF-8"
+outputEncoding = "UTF-8"
+documentTocDepth = 4
+pageTocDepth = 99
+numbering = true
