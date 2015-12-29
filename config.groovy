0792c9e18669b89b5a362d98aecfd4b841c88967
TGITS <thegeekintheshell@gmail.com>
2015-12-29 15:28:17 +0100

    Utilisation de Gaiden (https://github.com/kobo/gaiden).

---
+++ config.groovy
@@ -0,0 +1,11 @@
+// The title of this document
+//title = "Notes Lille AgileTour 2015"
+
+// The version of this document
+//version = "0.1"
+
+// The path of source files directory
+//sourceDirectoryPath = "."
+
+// The path of directory to be outputted a document
+//outputDirectoryPath = "build"
